4ab44b1608ba14eb3356b8c737e55bafe8f98869
Shadow15510 <antoine.royer31@orange.fr>
2020-08-26 14:24:10 +0200

    fix bug

--- weatherlib.py
+++ weatherlib.py
@@ -6,18 +6,18 @@ def get_weather(city_name, day):
   weather_data = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={api_key}").json()
 
   lon, lat = weather_data["coord"]["lon"], weather_data["coord"]["lat"]
-  weather_data = requests.get(f"https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude=current,minutely,hourly&appid={api_key}").json()["daily"][day]
+  weather_data = requests.get(f"https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude=current,minutely,hourly&appid={api_key}").json()
 
   return (
-    weather_data["weather"][0]["description"],
-    int(weather_data["temp"]["day"] - 273.15),
-    int(weather_data["feels_like"]["day"] - 273.15),
-    int(weather_data["dew_point"] - 273.15),
-    weather_data["pressure"],
-    weather_data["humidity"],
-    int(weather_data["wind_speed"] * 3.6),
-    weather_data["wind_deg"],
-    weather_data["clouds"],
-    weather_data["pop"] * 100,
+    weather_data["daily"][day]["weather"][0]["description"],
+    int(weather_data["daily"][day]["temp"]["day"] - 273.15),
+    int(weather_data["daily"][day]["feels_like"]["day"] - 273.15),
+    int(weather_data["daily"][day]["dew_point"] - 273.15),
+    weather_data["daily"][day]["pressure"],
+    weather_data["daily"][day]["humidity"],
+    int(weather_data["daily"][day]["wind_speed"] * 3.6),
+    weather_data["daily"][day]["wind_deg"],
+    weather_data["daily"][day]["clouds"],
+    weather_data["daily"][day]["pop"] * 100,
     weather_data["timezone"],
-    f'https://openweathermap.org/img/w/{weather_data["weather"][0]["icon"]}.png')
+    f'https://openweathermap.org/img/w/{weather_data["daily"][day]["weather"][0]["icon"]}.png')
