bb538a5b7fe6e74cb1135081d280c94aaab14dd0
Shadow15510 <antoine.royer31@orange.fr>
2020-09-23 22:02:25 +0200

    Fix bug

--- wikilib.py
+++ wikilib.py
@@ -8,6 +8,10 @@ def page_content(name, limit = 1000):
   
   def image_detect(img, code_html):
     img = [i for i in img if i.endswith(".jpg")]
+
+    print("Début du process : ")
+    for i in img: print(i)
+    
     for index, value in enumerate(img):
       value = value.split("/commons/")
       img[index] = value[0][6:] + "/commons/thumb/" + value[1]
@@ -32,6 +36,8 @@ def page_content(name, limit = 1000):
       summary = summary[:summary.find("==")]
       
     img = image_detect(search.images, search.html())
+    
+    print(f"Fin du process : {img}")
     
     return search.title, summary.replace(" , ", ", "), search.url, img, True
 
