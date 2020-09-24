fa35d6604a27368b2589d2a049053a0f7b93c3c4
Shadow15510 <antoine.royer31@orange.fr>
2020-09-24 22:49:17 +0200

    fix bug

--- wikibot.py
+++ wikibot.py
@@ -93,7 +93,7 @@ async def on_message(message):
     rep = discord.Embed(title=f"Help pannel (WikiBot v{__version__})", description="List of available commands", color=randint(0, 16777215))
     rep.add_field(name="Make a research on wikipedia", value="`/s < search_terms > [& < language >]`", inline=False)
     rep.add_field(name="Get an article from Wikipedia with the exact title", value="`/p < title > [& < language >]`", inline=False)
-    rep.add_field(name="Get an article with an automatic correction on the title", value="`/p+ < title > [& < language >]`", inline=False
+    rep.add_field(name="Get an article with an automatic correction on the title", value="`/p+ < title > [& < language >]`", inline=False)
     rep.add_field(name="Random selection of articles from Wikipedia", value="`/r < nb > [& < language >]`", inline=False)
     rep.add_field(name="Translate a text", value="`/t < text > [& < language >]`", inline=False)
     rep.add_field(name="Get some news", value="`/n < newspaper_name > [& < number_of_article >]`", inline=False)
