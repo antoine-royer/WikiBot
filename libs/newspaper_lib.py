473c37d8aacab9ed69ec2a71d1e3a103395a72d8
Shadow15510 <antoine.royer31@orange.fr>
2020-11-29 12:00:27 +0100

    Improve the auto-detection of wikipedia page's name

--- libs/newspaper_lib.py
+++ libs/newspaper_lib.py
@@ -2,95 +2,97 @@ import requests
 import xmltodict
 import html
 
-def special_char(text):
-  text = html.unescape(text)
 
-  text = text.replace("><", "> <")
-  start = text.find("<")
-  stop = text.find(">", start)
-  while start != -1:
-    text = text[:start] + text[stop + 1:]
+def special_char(text):
+    text = html.unescape(text)
+
+    text = text.replace("><", "> <")
     start = text.find("<")
     stop = text.find(">", start)
+    while start != -1:
+        text = text[:start] + text[stop + 1:]
+        start = text.find("<")
+        stop = text.find(">", start)
 
-  if len(text) > 1000: return text[:1000] + "…"
-  return text
+    if len(text) > 1000: return text[:1000] + "…"
+    return text
+
 
 class NewsPaper:
-  def __init__(self):
-    self.name = None
-    self.data = None
-    self.url = None
+    def __init__(self):
+        self.name = None
+        self.data = None
+        self.url = None
 
-  def __get_url(self):
-    rss = (
-      "http://www.thelancet.com/rssfeed/lancet_current.xml",
-      "https://www.lemonde.fr/rss/une.xml",
-      "https://www.lexpress.fr/rss/alaune.xml",
-      "https://www.lefigaro.fr/rss/figaro_actualites.xml",
-      "https://www.nouvelobs.com/a-la-une/rss.xml",
-      "https://time.com/rss",
-      "https://rss.nytimes.com/services/xml/rss/nyt/World.xml",
-      "https://www.courrierinternational.com/feed/all/rss.xml",
-      "http://rss.liberation.fr/rss/9/",
-      "https://www.monde-diplomatique.fr/rss",
-      "https://www.theguardian.com/international/rss",
-      "https://www.sciencesetavenir.fr/rss.xml")
+    def __get_url(self):
+        rss = (
+            "http://www.thelancet.com/rssfeed/lancet_current.xml",
+            "https://www.lemonde.fr/rss/une.xml",
+            "https://www.lexpress.fr/rss/alaune.xml",
+            "https://www.lefigaro.fr/rss/figaro_actualites.xml",
+            "https://www.nouvelobs.com/a-la-une/rss.xml",
+            "https://time.com/rss",
+            "https://rss.nytimes.com/services/xml/rss/nyt/World.xml",
+            "https://www.courrierinternational.com/feed/all/rss.xml",
+            "http://rss.liberation.fr/rss/9/",
+            "https://www.monde-diplomatique.fr/rss",
+            "https://www.theguardian.com/international/rss",
+            "https://www.sciencesetavenir.fr/rss.xml")
 
-    if self.index < len(rss):
-      return rss[self.index]
-    return None
-
-  def __name_detect(self):
-    newspapers_name = (
-      "the lancet#lancet",
-      "le monde#monde",
-      "l'express#express",
-      "le figaro#figaro",
-      "l'obs#obs#l'observateur#observateur",
-      "the time#time",
-      "the news york times#new york times#ny times",
-      "courrier international",
-      "libération#liberation#libe#libé",
-      "le monde diplomatique#monde diplomatique",
-      "the guardian#guardian",
-      "sciences et avenir")
-    
-    for index, name in enumerate(newspapers_name):
-      if self.name in name.split("#"):
-        self.index = index
+        if self.index < len(rss):
+            return rss[self.index]
         return None
 
-    return [name.split("#")[0].title() for name in newspapers_name]
-    
-  def __get_data(self):
-    return xmltodict.parse(requests.get(self.url).content)
+    def __name_detect(self):
+        newspapers_name = (
+            "the lancet#lancet",
+            "le monde#monde",
+            "l'express#express",
+            "le figaro#figaro",
+            "l'obs#obs#l'observateur#observateur",
+            "the time#time",
+            "the news york times#new york times#ny times",
+            "courrier international",
+            "libération#liberation#libe#libé",
+            "le monde diplomatique#monde diplomatique",
+            "the guardian#guardian",
+            "sciences et avenir")
+        
+        for index, name in enumerate(newspapers_name):
+            if self.name in name.split("#"):
+                self.index = index
+                return None
 
-  def get_rss(self, name, nb, plus):
-    
-    self.name = name.lower()
-    
-    test_available = self.__name_detect()
-    if test_available: return None, test_available
-    
-    self.url = self.__get_url()
-    self.data = self.__get_data()
+        return [name.split("#")[0].title() for name in newspapers_name]
+        
+    def __get_data(self):
+        return xmltodict.parse(requests.get(self.url).content)
 
-    if self.index == 0: self.data = self.__the_lancet(nb)
-    elif self.index == 1: self.data = self.__le_monde(nb)
-    elif self.index == 2: self.data = self.__l_express(nb)
-    elif self.index == 3: self.data = self.__le_figaro(nb)
-    elif self.index == 4: self.data = self.__l_obs(nb)
-    elif self.index == 5: self.data = self.__time(nb)
-    elif self.index == 6: self.data = self.__the_new_york_times(nb)
-    elif self.index == 7: self.data = self.__courrier_international(nb)   
-    elif self.index == 8: self.data = self.__liberation(nb)
-    elif self.index == 9: self.data = self.__le_monde_diplomatique(nb)
-    elif self.index == 10: self.data = self.__the_guardian(nb)
-    elif self.index == 11: self.data = self.__sciences_et_avenir(nb)
+    def get_rss(self, name, nb, plus):
+        
+        self.name = name.lower()
+        
+        test_available = self.__name_detect()
+        if test_available: return None, test_available
+        
+        self.url = self.__get_url()
+        self.data = self.__get_data()
 
-    if plus: return [self.data[-1]], None
-    else: return self.data, None
+        if self.index == 0: self.data = self.__the_lancet(nb)
+        elif self.index == 1: self.data = self.__le_monde(nb)
+        elif self.index == 2: self.data = self.__l_express(nb)
+        elif self.index == 3: self.data = self.__le_figaro(nb)
+        elif self.index == 4: self.data = self.__l_obs(nb)
+        elif self.index == 5: self.data = self.__time(nb)
+        elif self.index == 6: self.data = self.__the_new_york_times(nb)
+        elif self.index == 7: self.data = self.__courrier_international(nb)   
+        elif self.index == 8: self.data = self.__liberation(nb)
+        elif self.index == 9: self.data = self.__le_monde_diplomatique(nb)
+        elif self.index == 10: self.data = self.__the_guardian(nb)
+        elif self.index == 11: self.data = self.__sciences_et_avenir(nb)
+
+        if plus: return [self.data[-1]], None
+        else: return self.data, None
 
 # --- self.data
 # 0 : Titre
@@ -101,128 +103,128 @@ class NewsPaper:
 
 # --- Newspapers functions --- #
 
-  def __the_lancet(self, nb):
-    information = []
-    for news in self.data["rdf:RDF"]["item"][0:nb]:
-      information.append([special_char(news["title"]),
-                          special_char(news["description"]),
-                          news["link"],
-                          None])
-    return information
+    def __the_lancet(self, nb):
+        information = []
+        for news in self.data["rdf:RDF"]["item"][0:nb]:
+            information.append([special_char(news["title"]),
+                                                    special_char(news["description"]),
+                                                    news["link"],
+                                                    None])
+        return information
 
-  def __le_monde(self, nb):
-    information = []
-    for news in self.data["rss"]["channel"]["item"][0:nb]:
-      information.append([special_char(news["title"]),
-                          special_char(news["description"]),
-                          news["link"],
-                          news["media:content"]["@url"]])
-    return information
+    def __le_monde(self, nb):
+        information = []
+        for news in self.data["rss"]["channel"]["item"][0:nb]:
+            information.append([special_char(news["title"]),
+                                                    special_char(news["description"]),
+                                                    news["link"],
+                                                    news["media:content"]["@url"]])
+        return information
 
-  def __l_express(self, nb):
-    information = []
-    for news in self.data["rss"]["channel"]["item"][0:nb]:
-      information.append([f"[{news['subhead']}] {special_char(news['title'])}",
-                          special_char(news["description"]),
-                          news["link"],
-                          news["enclosure"]["@url"]])
-    return information
+    def __l_express(self, nb):
+        information = []
+        for news in self.data["rss"]["channel"]["item"][0:nb]:
+            information.append([f"[{news['subhead']}] {special_char(news['title'])}",
+                                                    special_char(news["description"]),
+                                                    news["link"],
+                                                    news["enclosure"]["@url"]])
+        return information
 
-  def __le_figaro(self, nb):
-    information = []
-    for news in self.data["rss"]["channel"]["item"][0:nb]:
-      summary = news["description"]
-      if not summary: summary = "*Unavailable*"
-      information.append([f"[{news['category']}] {special_char(news['title'])}",
-                          special_char(summary),
-                          news["link"],
-                          None])
-    return information
+    def __le_figaro(self, nb):
+        information = []
+        for news in self.data["rss"]["channel"]["item"][0:nb]:
+            summary = news["description"]
+            if not summary: summary = "*Unavailable*"
+            information.append([f"[{news['category']}] {special_char(news['title'])}",
+                                                    special_char(summary),
+                                                    news["link"],
+                                                    None])
+        return information
 
-  def __l_obs(self, nb):
-    information = []
-    for news in self.data["rss"]["channel"]["item"][0:nb]:
-      information.append([f"[{news['category']}] {special_char(news['title'])}",
-                          special_char(news["description"]),
-                          news["link"],
-                          news["enclosure"]["@url"]])
-    return information
+    def __l_obs(self, nb):
+        information = []
+        for news in self.data["rss"]["channel"]["item"][0:nb]:
+            information.append([f"[{news['category']}] {special_char(news['title'])}",
+                                                    special_char(news["description"]),
+                                                    news["link"],
+                                                    news["enclosure"]["@url"]])
+        return information
 
-  def __time(self, nb):
-    information = []
-    for news in self.data["rss"]["channel"]["item"][0:nb]:
-      information.append([special_char(news["title"]),
-                          special_char(news["description"]),
-                          news["link"],
-                          None])
-    return information
+    def __time(self, nb):
+        information = []
+        for news in self.data["rss"]["channel"]["item"][0:nb]:
+            information.append([special_char(news["title"]),
+                                                    special_char(news["description"]),
+                                                    news["link"],
+                                                    None])
+        return information
 
-  def __the_new_york_times(self, nb):
-    information = []
-    for news in self.data["rss"]["channel"]["item"][0:nb]:
-      information.append([special_char(news["title"]),
-                          special_char(news["description"]),
-                          news["link"],
-                          news["media:content"]["@url"]])
-    return information
+    def __the_new_york_times(self, nb):
+        information = []
+        for news in self.data["rss"]["channel"]["item"][0:nb]:
+            information.append([special_char(news["title"]),
+                                                    special_char(news["description"]),
+                                                    news["link"],
+                                                    news["media:content"]["@url"]])
+        return information
 
-  def __courrier_international(self, nb):
-    information = []
-    for news in self.data["rss"]["channel"]["item"][0:nb]:
-      information.append([special_char(news["title"]),
-                          special_char(news["description"]),
-                          news["link"],
-                          None])
-    return information
+    def __courrier_international(self, nb):
+        information = []
+        for news in self.data["rss"]["channel"]["item"][0:nb]:
+            information.append([special_char(news["title"]),
+                                                    special_char(news["description"]),
+                                                    news["link"],
+                                                    None])
+        return information
 
-  def __liberation(self, nb):
-    information = []
-    for news in self.data["feed"]["entry"][0:nb]:
-      try:
-        information.append([f"[{news['category']['@term']}] {special_char(news['title'])}",
-                           special_char(news["summary"]["#text"]),
-                           news["link"][0]["@href"],
-                           news["link"][1]["@href"]])
-      except:
-        information.append([f"[{news['category']['@term']}] {special_char(news['title'])}",
-                           "*Unavailable*",
-                           news["link"][0]["@href"],
-                           news["link"][1]["@href"]])
-    return information
+    def __liberation(self, nb):
+        information = []
+        for news in self.data["feed"]["entry"][0:nb]:
+            try:
+                information.append([f"[{news['category']['@term']}] {special_char(news['title'])}",
+                                                     special_char(news["summary"]["#text"]),
+                                                     news["link"][0]["@href"],
+                                                     news["link"][1]["@href"]])
+            except:
+                information.append([f"[{news['category']['@term']}] {special_char(news['title'])}",
+                                                     "*Unavailable*",
+                                                     news["link"][0]["@href"],
+                                                     news["link"][1]["@href"]])
+        return information
 
-  def __le_monde_diplomatique(self, nb):
-    information = []
-    for news in self.data["rss"]["channel"]["item"][0:nb]:
-      summary = special_char(news["description"])
-      summary = summary[:min(summary.find("/"), summary.find("(...)") + 5)]
-      if not summary: summary = "*Unavailable*"
+    def __le_monde_diplomatique(self, nb):
+        information = []
+        for news in self.data["rss"]["channel"]["item"][0:nb]:
+            summary = special_char(news["description"])
+            summary = summary[:min(summary.find("/"), summary.find("(...)") + 5)]
+            if not summary: summary = "*Unavailable*"
 
-      information.append([special_char(news["title"]),
-                         summary,
-                         news["link"],
-                         None])
-    return information
+            information.append([special_char(news["title"]),
+                                                 summary,
+                                                 news["link"],
+                                                 None])
+        return information
 
-  def __the_guardian(self, nb):
-    information = []
-    for news in self.data["rss"]["channel"]["item"][0:nb]:
-      information.append([special_char(news["title"]),
-                          special_char(news["description"]),
-                          news["link"],
-                          news["media:content"][0]["@url"]])
-    return information
+    def __the_guardian(self, nb):
+        information = []
+        for news in self.data["rss"]["channel"]["item"][0:nb]:
+            information.append([special_char(news["title"]),
+                                                    special_char(news["description"]),
+                                                    news["link"],
+                                                    news["media:content"][0]["@url"]])
+        return information
 
-  def __sciences_et_avenir(self, nb):
-    information = []
-    for news in self.data["rss"]["channel"]["item"][0:nb]:
-      image = None
-      if "enclosure" in news.keys(): image = news["enclosure"]["@url"]
-      information.append([f"[{news['category']}] {special_char(news['title'])}",
-                          special_char(news["description"]),
-                          news["link"],
-                          image])
-    return information
-                         
+    def __sciences_et_avenir(self, nb):
+        information = []
+        for news in self.data["rss"]["channel"]["item"][0:nb]:
+            image = None
+            if "enclosure" in news.keys(): image = news["enclosure"]["@url"]
+            information.append([f"[{news['category']}] {special_char(news['title'])}",
+                                                    special_char(news["description"]),
+                                                    news["link"],
+                                                    image])
+        return information
+                                                 
 
 
-                         
+                                                 
--- wikibot.py
+++ wikibot.py
@@ -19,101 +19,101 @@ __version__ = "1.9.2"
 
 
 def make_embed(title, description, field, color, image, in_line = False, thumb = False):
-	if not color: color = randint(0, 16777215)
-	answer = discord.Embed(title=title, description=description, color=color)
+    if not color: color = randint(0, 16777215)
+    answer = discord.Embed(title=title, description=description, color=color)
 
-	for i in field:
-		answer.add_field(name=i[0], value=i[1], inline=in_line)
-	
-	if image:
-		if thumb: answer.set_thumbnail(url=image)
-		else: answer.set_image(url=image)
-	return answer
+    for i in field:
+        answer.add_field(name=i[0], value=i[1], inline=in_line)
+    
+    if image:
+        if thumb: answer.set_thumbnail(url=image)
+        else: answer.set_image(url=image)
+    return answer
 
 @client.event
 async def on_message(message):
-	msg_content, rep = message.content, None
+    msg_content, rep = message.content, None
 
-	if message.author == client.user: return None
+    if message.author == client.user: return None
 
-	try:
-		if msg_content[0] != "/": return None
-	except:
-		return None
+    try:
+        if msg_content[0] != "/": return None
+    except:
+        return None
 
-	msg_content = list(msg_content[1:].partition("&"))
+    msg_content = list(msg_content[1:].partition("&"))
 
-	msg_content[0] = msg_content[0].rstrip()
+    msg_content[0] = msg_content[0].rstrip()
 
-	language = msg_content[2].strip().rstrip()
-	
-	if not language:
-		language = "en"
-	wl.wikipedia.set_lang(language.split()[0])
+    language = msg_content[2].strip().rstrip()
+    
+    if not language:
+        language = "en"
+    wl.wikipedia.set_lang(language.split()[0])
 
-	if not msg_content[0].find("r "):
-		rep = make_embed(*wl.page_random(msg_content[0][2:]))
-		
-	elif not msg_content[0].find("p+ "):
-		rep = make_embed(*wl.page_read(msg_content[0][2:], True))
+    if not msg_content[0].find("r "):
+        rep = make_embed(*wl.page_random(msg_content[0][2:]))
+        
+    elif not msg_content[0].find("p+ "):
+        rep = make_embed(*wl.page_read(msg_content[0][2:], True))
 
-	elif not msg_content[0].find("p "):
-		rep = make_embed(*wl.page_read(msg_content[0][2:]))
+    elif not msg_content[0].find("p "):
+        rep = make_embed(*wl.page_read(msg_content[0][2:]))
 
-	elif not msg_content[0].find("s "):
-		rep = make_embed(*wl.page_search(msg_content[0][2:]))
-	
-	elif not msg_content[0].find("e "):
-		rep = wl.eliza_call(msg_content[0][2:])
+    elif not msg_content[0].find("s "):
+        rep = make_embed(*wl.page_search(msg_content[0][2:]))
+    
+    elif not msg_content[0].find("e "):
+        rep = wl.eliza_call(msg_content[0][2:])
 
-	elif not msg_content[0].find("w "):
-		city_name = msg_content[0][2:]
-		rep, img, day, timezone, datetime = wl.weather(city_name, language)
-		
-		if not rep:
-			rep = make_embed("Weather", "Unknown city's name", [("Error", f"No city were found for the name : '{city_name}'. Please check the city's name.")], 16711680, None)
-		else:
-			if day == 0: day = f"today : {datetime}"
-			elif day == 1: day = f"tomorrow : {datetime}"
-			else: day = f"in {day} days : {datetime}"
-			rep = make_embed("Weather", f"{city_name} {day} ({timezone})", rep, None, img, True, True)
+    elif not msg_content[0].find("w "):
+        city_name = msg_content[0][2:]
+        rep, img, day, timezone, datetime = wl.weather(city_name, language)
+        
+        if not rep:
+            rep = make_embed("Weather", "Unknown city's name", [("Error", f"No city were found for the name : '{city_name}'. Please check the city's name.")], 16711680, None)
+        else:
+            if day == 0: day = f"today : {datetime}"
+            elif day == 1: day = f"tomorrow : {datetime}"
+            else: day = f"in {day} days : {datetime}"
+            rep = make_embed("Weather", f"{city_name} {day} ({timezone})", rep, None, img, True, True)
 
-			rep.set_footer(text = "Weather forecast provided by OpenWeather", icon_url = "https://openweathermap.org/themes/openweathermap/assets/img/logo_white_cropped.png")
+            rep.set_footer(text = "Weather forecast provided by OpenWeather", icon_url = "https://openweathermap.org/themes/openweathermap/assets/img/logo_white_cropped.png")
 
-	elif not msg_content[0].find("n "):
-		name, news = wl.get_news(msg_content[0][2:], language)
-		embed_title = f"**{name}**"
-		if news[0]:
-			news = news[0]
-			rep = []
-			for article in news:
-				rep.append(make_embed(embed_title, article[0], (("Summary", article[1]), ("Link", article[2])), None, article[3]))
-		else:
-			rep = make_embed(embed_title, "Unknown newspaper", (("Error", "The newspaper requested isn't registrated"), ("Newspapers available", " - ".join(news[1]))), 16711680, None)
-		
-	elif msg_content[0] == "help":
-		rep = discord.Embed(title=f"Help pannel (WikiBot v{__version__})", description="List of available commands", color=randint(0, 16777215))
-		rep.add_field(name="Make a research on wikipedia", value="`/s < search_terms > [& < language >]`", inline=False)
-		rep.add_field(name="Get an article from Wikipedia with the exact title", value="`/p < title > [& < language >]`", inline=False)
-		rep.add_field(name="Get an article with an automatic correction on the title", value="`/p+ < title > [& < language >]`", inline=False)
-		rep.add_field(name="Random selection of articles from Wikipedia", value="`/r < nb > [& < language >]`", inline=False)
-		rep.add_field(name="Get some news", value="`/n < newspaper_name > [& < number_of_article > [+]]`", inline=False)
-		rep.add_field(name="Get the weather", value="`/w < city name > [& < day_of_forecast >]` for the day : 0 is today, 1 tomorrow…", inline=False)
-		rep.add_field(name="Talk with Eliza", value="`/e < message >`", inline=False)
-		rep.add_field(name="Complete documentation", value="https://github.com/Shadow15510/WikiBot/blob/master/README.md", inline=False)
+    elif not msg_content[0].find("n "):
+        name, news = wl.get_news(msg_content[0][2:], language)
+        embed_title = f"**{name}**"
+        if news[0]:
+            news = news[0]
+            rep = []
+            for article in news:
+                rep.append(make_embed(embed_title, article[0], (("Summary", article[1]), ("Link", article[2])), None, article[3]))
+        else:
+            rep = make_embed(embed_title, "Unknown newspaper", (("Error", "The newspaper requested isn't registrated"), ("Newspapers available", " - ".join(news[1]))), 16711680, None)
+        
+    elif msg_content[0] == "help":
+        rep = discord.Embed(title=f"Help pannel (WikiBot v{__version__})", description="List of available commands", color=randint(0, 16777215))
+        rep.add_field(name="Make a research on wikipedia", value="`/s < search_terms > [& < language >]`", inline=False)
+        rep.add_field(name="Get an article from Wikipedia with the exact title", value="`/p < title > [& < language >]`", inline=False)
+        rep.add_field(name="Get an article with an automatic correction on the title", value="`/p+ < title > [& < language >]`", inline=False)
+        rep.add_field(name="Random selection of articles from Wikipedia", value="`/r < nb > [& < language >]`", inline=False)
+        rep.add_field(name="Get some news", value="`/n < newspaper_name > [& < number_of_article > [+]]`", inline=False)
+        rep.add_field(name="Get the weather", value="`/w < city name > [& < day_of_forecast >]` for the day : 0 is today, 1 tomorrow…", inline=False)
+        rep.add_field(name="Talk with Eliza", value="`/e < message >`", inline=False)
+        rep.add_field(name="Complete documentation", value="https://github.com/Shadow15510/WikiBot/blob/master/README.md", inline=False)
 
-	if not rep: return None
-	
-	if type(rep) == str:
-		await message.channel.send(rep)
-	elif type(rep) == list:
-		for msg in rep: await message.channel.send(embed = msg)
-	else:
-		await message.channel.send(embed = rep)
+    if not rep: return None
+    
+    if type(rep) == str:
+        await message.channel.send(rep)
+    elif type(rep) == list:
+        for msg in rep: await message.channel.send(embed = msg)
+    else:
+        await message.channel.send(embed = rep)
 
 @client.event
 async def on_ready():
-	await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="/help"))
-	print("Online.")
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="/help"))
+    print("Online.")
 
 client.run(token)
