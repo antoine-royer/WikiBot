eda3b5e59824dae1ca28eacda4888aba105a0c34
Shadow15510 <antoine.royer14@orange.fr>
2022-06-21 15:10:55 +0200

    Fix bugs

--- wikibot.py
+++ wikibot.py
@@ -20,6 +20,7 @@ client = commands.Bot(command_prefix=".", intents=intents)
 slash = discord_slash.SlashCommand(client, sync_commands=True)
 token = os.environ["token"]
 __version__ = "2.2.0"
+guild_ids = [658281779408535552, 685936220395929600, 688378964636336128, 694107785574613003, 750778113503264938, 774980578621259826, 780134704962601022]
 
 
 def make_embed(title, description, field, color, image, in_line = False, thumb = False):
@@ -41,67 +42,61 @@ async def on_ready():
     print("Online.")
     client.add_cog(WikiBot(client))
 
-    
-class WikiBot(commands.Cog):
-    def __init__(self, bot):
-        self.guild_ids = []
-        for guild in client.guilds:
-            guild_ids.append(guild.id)
 
-    @slash.slash(name="r", description="Random selection of articles from Wikipedia", guild_ids=self.guild_ids)
-    async def r(ctx, number: int, language: str="en"):
-        wl.wikipedia.set_lang(language.split()[0])
-        await ctx.send(embed=make_embed(*wl.page_random(number)))
+@slash.slash(name="r", description="Random selection of articles from Wikipedia", guild_ids=guild_ids)
+async def r(ctx, number: int, language: str="en"):
+    wl.wikipedia.set_lang(language.split()[0])
+    await ctx.send(embed=make_embed(*wl.page_random(number)))
 
 
-    @slash.slash(name="p-auto", description="Get an article with an automatic correction on the title", guild_ids=self.guild_ids)
-    async def pauto(ctx, research: str, language: str="en"):
-        wl.wikipedia.set_lang(language.split()[0])
-        await ctx.send(embed=make_embed(*wl.page_read(research, True)))
+@slash.slash(name="p-auto", description="Get an article with an automatic correction on the title", guild_ids=guild_ids)
+async def pauto(ctx, research: str, language: str="en"):
+    wl.wikipedia.set_lang(language.split()[0])
+    await ctx.send(embed=make_embed(*wl.page_read(research, True)))
 
 
-    @slash.slash(name="p", description="Get an article from Wikipedia with the exact title", guild_ids=self.guild_ids)
-    async def p(ctx, research: str, language: str="en"):
-        wl.wikipedia.set_lang(language.split()[0])
-        await ctx.send(embed=make_embed(*wl.page_read(research)))
+@slash.slash(name="p", description="Get an article from Wikipedia with the exact title", guild_ids=guild_ids)
+async def p(ctx, research: str, language: str="en"):
+    wl.wikipedia.set_lang(language.split()[0])
+    await ctx.send(embed=make_embed(*wl.page_read(research)))
 
 
-    @slash.slash(name="s", description="Make a research on wikipedia", guild_ids=self.guild_ids)
-    async def s(ctx, research: str, language: str="en"):
-        wl.wikipedia.set_lang(language.split()[0])
-        await ctx.send(embed=make_embed(*wl.page_search(research)))
+@slash.slash(name="s", description="Make a research on wikipedia", guild_ids=guild_ids)
+async def s(ctx, research: str, language: str="en"):
+    wl.wikipedia.set_lang(language.split()[0])
+    await ctx.send(embed=make_embed(*wl.page_search(research)))
 
 
-    @slash.slash(name="w", description="Get the weather", guild_ids=self.guild_ids)
-    async def w(ctx, city_name: str, forecast: int=1):
-        rep, img, day, timezone, datetime = wl.weather(city_name, forecast)
-        if not rep:
-            rep = make_embed("Weather", "Unknown city's name", [("Error", f"No city were found for the name : '{city_name}'. Please check the city's name.")], 16711680, None)
+@slash.slash(name="w", description="Get the weather", guild_ids=guild_ids)
+async def w(ctx, city_name: str, forecast: int=1):
+    rep, img, day, timezone, datetime = wl.weather(city_name, forecast)
+    if not rep:
+        rep = make_embed("Weather", "Unknown city's name", [("Error", f"No city were found for the name : '{city_name}'. Please check the city's name.")], 16711680, None)
+    else:
+        if day == 0: day = f"today : {datetime}"
+        elif day == 1: day = f"tomorrow : {datetime}"
+        else: day = f"in {day} days : {datetime}"
+        rep = make_embed("Weather", f"{city_name} {day} ({timezone})", rep, None, img, True, True)
+
+        rep.set_footer(text = "Weather forecast provided by OpenWeather", icon_url = "https://openweathermap.org/themes/openweathermap/assets/img/logo_white_cropped.png")
+    await ctx.send(embed=rep)
+
+
+@slash.slash(name="n", description="Get some news", guild_ids=guild_ids)
+async def n(ctx, newspaper: str, nb_article: int=1, is_selected: bool=False):
+    name, news, selection = wl.get_news(newspaper, nb_article, is_selected)
+    embed_title = f"**{name}**"
+    if news[0]:
+        news = news[0]
+        rep = []
+        if not selection:
+            for index, article in enumerate(news):
+                rep.append(make_embed(f"{embed_title} (#{index + 1})", article[0], (("Summary", article[1]), ("Link", article[2])), None, article[3]))
         else:
-            if day == 0: day = f"today : {datetime}"
-            elif day == 1: day = f"tomorrow : {datetime}"
-            else: day = f"in {day} days : {datetime}"
-            rep = make_embed("Weather", f"{city_name} {day} ({timezone})", rep, None, img, True, True)
-
-            rep.set_footer(text = "Weather forecast provided by OpenWeather", icon_url = "https://openweathermap.org/themes/openweathermap/assets/img/logo_white_cropped.png")
-        await ctx.send(embed=rep)
-
-
-    @slash.slash(name="n", description="Get some news", guild_ids=self.guild_ids)
-    async def n(ctx, newspaper: str, nb_article: int=1, is_selected: bool=False):
-        name, news, selection = wl.get_news(newspaper, nb_article, is_selected)
-        embed_title = f"**{name}**"
-        if news[0]:
-            news = news[0]
-            rep = []
-            if not selection:
-                for index, article in enumerate(news):
-                    rep.append(make_embed(f"{embed_title} (#{index + 1})", article[0], (("Summary", article[1]), ("Link", article[2])), None, article[3]))
-            else:
-                rep.append(make_embed(f"{embed_title} (#{selection})", news[0][0], (("Summary", news[0][1]), ("Link", news[0][2])), None, news[0][3]))
-        else:
-            rep = make_embed(embed_title, "Unknown newspaper", (("Error", "The newspaper requested isn't registrated"), ("Newspapers available", " - ".join(news[1]))), 16711680, None)
-        for article in rep: await ctx.send(embed=article)
+            rep.append(make_embed(f"{embed_title} (#{selection})", news[0][0], (("Summary", news[0][1]), ("Link", news[0][2])), None, news[0][3]))
+    else:
+        rep = make_embed(embed_title, "Unknown newspaper", (("Error", "The newspaper requested isn't registrated"), ("Newspapers available", " - ".join(news[1]))), 16711680, None)
+    for article in rep: await ctx.send(embed=article)
 
 
 client.run(token)
